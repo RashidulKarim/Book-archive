6f1eb6962c6780be5aca25095dd1127333a90b6e
rashidulkarim <rashidul.karim7@gmail.com>
2021-09-02 13:31:19 +0600

    done the project

--- js/app.js
+++ js/app.js
@@ -1,61 +1,50 @@
-const noOfResult = document.getElementById('noOfResultFound');
-const booksContainer = document.getElementById("books-container");
+//capture search field
 const searchField = document.getElementById("search-field");
-noOfResult.style.color = "gray"
+//capture search result peragraph
+const noOfResult = document.getElementById('noOfResultFound');
+//capture book container div
+const booksContainer = document.getElementById("books-container");
 
-// looding spinner 
+//  conditionally show and hide spinner 
     const spinner = (spinnerStatus, containerStatus) => {
         document.getElementById('spinner').style.display= spinnerStatus;
         booksContainer.style.display = containerStatus;
     }
 
-
+//get search data from user
 const getInputValue = () => {
-    //capture search data
     const searchText = searchField.value;
     booksContainer.textContent = "";
     noOfResult.textContent = "";
     if(searchText.length){
-         // show the spinner 
     spinner("block", "none")
-        // call api with search data
     getData(searchText) 
     }
     else{
         noOfResult.innerText = `Search box can't be empty. Please write something.`;
     }
-    
 }
-
+// load data from api 
 const getData = async(searchText) =>{
-    const url = `http://openlibrary.org/search.json?q=${searchText}`
+    const url = `https://openlibrary.org/search.json?q=${searchText}`
     const res = await fetch(url);
     const data = await res.json()
-
-
-    if(data.docs.length){
-         //call displayData for showing the result.
-         spinner("none", "grid")
-    displayData(data.docs)    
     const filterBook = data.docs.filter(book => book.title && book.cover_i && book.author_name && book.first_publish_year && book.publisher)
-    
-    //show No Of Result Found
+    if(filterBook.length){
+    spinner("none", "grid") 
+    displayData(filterBook)    
+    //show Number Of Result Found
     noOfResult.innerHTML = `${data?.numFound} results for <span class='font-bold'>"${searchText}"</span>.Showing ${filterBook.length} of ${data?.numFound} .`
     searchField.value = '';
     }
     else{
         noOfResult.innerHTML = `No Result Found For <span class='font-bold'>"${searchText}"</span>`
-            // show the spinner 
-    spinner("none", "grid")
+        spinner("none", "grid")
     }
-   
-
 }
-
+    // show books on ui
 const displayData = (data) =>{
     data.forEach(book=> {
-        
-        if(book.title && book.cover_i && book.author_name && book.first_publish_year && book.publisher){
             const bookContainer = document.createElement('div');
             bookContainer.classList.add("p-4","rounded-lg","border-2","border-red-500")
             bookContainer.innerHTML = `
@@ -63,14 +52,13 @@ const displayData = (data) =>{
             <img style="width:120%; height: 350px;" src ="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"/>
             </div>
             <div id= "book-description">
-            <h2 class="text-3xl text-purple-700 font-bold mt-2">${book.title.slice(0,15)}</h2>
-            <p class="text-gray-500">By ${book.author_name[0]}</p>
-            <p>First Publish Year: ${book.first_publish_year}</p>
-            <p>Publisher: ${book.publisher[0].slice(0,20)}</p>
+            <h2 class="text-3xl text-purple-700 font-bold mt-2 mb-1">${book.title.slice(0,15)}</h2>
+            <p class="text-gray-500 font-bold">By ${book.author_name[0]}</p>
+            <p>First Publish: <span class="font-bold">${book.first_publish_year}</span></p>
+            <p>Publisher: <span class="font-bold">${book.publisher[0].slice(0,18)}</span></p>
             </div>
             `   
             booksContainer.appendChild(bookContainer);
-            }})
-            
+            }) 
         }
   
